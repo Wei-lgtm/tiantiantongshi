da01d4aa3392e332138f77e351d0aa461d26aba3
pangpangyu <1047510535@qq.com>
2019-06-16 14:18:35 +0800

    ...

--- utils/request.js
+++ utils/request.js
@@ -14,10 +14,10 @@ const time = new Date().getTime()
 let sign = time + md5('@@@www.lumibayedu.com!@#')
 sign = sha256(md5(sign))
 
-// server.defaults.headers.common['sign'] = sign
-// server.defaults.headers.common['platform'] = 'pc_teach'
-// server.defaults.headers.common['version'] = '1.0.0'
-// server.defaults.headers.common['accessTime'] = time
+server.defaults.headers.common['sign'] = sign
+server.defaults.headers.common['platform'] = 'pc_teach'
+server.defaults.headers.common['version'] = '1.0.0'
+server.defaults.headers.common['accessTime'] = time
 // server.defaults.headers.common['uid'] = 0
 
 server.interceptors.request.use(
@@ -25,12 +25,12 @@ server.interceptors.request.use(
         if (config.method == 'get') {
             config.data = true
         }
-        // let userInfo = {id:0}
-        // if(sessionStorage.getItem("SUCCESS")){
-        //     userInfo = JSON.parse( aesDecrypt(sessionStorage.getItem("SUCCESS") , 'abc') )
-        // }
-        // config.headers.common['accessToken'] = sessionStorage.getItem("token") || '';
-        // config.headers.common['uid'] =  userInfo.id
+        let userInfo = {id:0}
+        if(sessionStorage.getItem("SUCCESS")){
+            userInfo = JSON.parse( aesDecrypt(sessionStorage.getItem("SUCCESS") , 'abc') )
+        }
+        config.headers.common['accessToken'] = sessionStorage.getItem("token") || '';
+        config.headers.common['uid'] =  userInfo.id
         return config
     },
     error => {
